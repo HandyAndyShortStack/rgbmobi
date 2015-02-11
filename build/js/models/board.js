1f6acb9b4d0ae65a73593db9372e653a8e326296
Andy Short <andynshort@gmail.com>
2015-02-10 18:05:33 -0800

    fix typo

--- build/js/models/board.js
+++ build/js/models/board.js
@@ -15,7 +15,7 @@
         }
         return _results;
       })(),
-      rows: [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [6, 4, 7]],
+      rows: [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [6, 4, 2]],
       lcm: function() {
         var exponent, prime, primes, value, _i, _len, _ref, _ref1, _square;
         primes = {};
